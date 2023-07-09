d0215780c66731df14b10c1931e778588c7c1dee
WeiTsungCheng <forwindsun1359@gmail.com>
2023-07-09 15:40:03 +0800

    Initial Commit

---
+++ EssentialFeedStudy/EssentialFeedStudy.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedStudy.h
+//  EssentialFeedStudy
+//
+//  Created by WEI-TSUNG CHENG on 2023/7/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedStudy.
+FOUNDATION_EXPORT double EssentialFeedStudyVersionNumber;
+
+//! Project version string for EssentialFeedStudy.
+FOUNDATION_EXPORT const unsigned char EssentialFeedStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedStudy/PublicHeader.h>
+
+
