475a3f3437553c590337e40a38435a955f97a484
WeiTsungCheng <forwindsun1359@gmail.com>
2023-07-10 08:44:36 +0800

    move RemoteFeedLoader to production

---
+++ EssentialFeedStudy/Feed Feature/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeedStudy
+//
+//  Created by WEI-TSUNG CHENG on 2023/7/10.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    
+    func get(from url: URL)
+
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+  
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- EssentialFeedStudyTests/RmoteFeedLoaderTests.swift
+++ EssentialFeedStudyTests/RmoteFeedLoaderTests.swift
@@ -7,30 +7,7 @@
 
 import XCTest
 import Foundation
-
-class RemoteFeedLoader {
-    
-    let client: HTTPClient
-    let url: URL
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    
-    func get(from url: URL)
-
-}
-
-
-
-
+import EssentialFeedStudy
 
 class RemoteFeedLoaderTests: XCTestCase {
     
@@ -60,12 +37,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        
-       func get(from url: URL) {
-            requestedURL = url
-        }
-        
+    
         var requestedURL: URL?
+        
+        func get(from url: URL) {
+             requestedURL = url
+         }
+         
         
     }
 }
