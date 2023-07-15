6894751c4c347ac44758ed090b2814956af1ebb7
WeiTsungCheng <forwindsun1359@gmail.com>
2023-07-15 13:04:43 +0800

    Simply type declarations with helper function

--- EssentialFeedStudyTests/Feed API/RmoteFeedLoaderTests.swift
+++ EssentialFeedStudyTests/Feed API/RmoteFeedLoaderTests.swift
@@ -41,8 +41,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-            
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
+        
+        expect(sut, toCompleteWith: failure(.connectivity)) {
             
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
@@ -57,7 +57,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         samples.enumerated().forEach { index, code in
             
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
@@ -70,7 +70,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnHTTP200ResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWith: failure(.invalidData)) {
             
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
@@ -135,6 +135,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeak(client, file: file, line: line)
         
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
