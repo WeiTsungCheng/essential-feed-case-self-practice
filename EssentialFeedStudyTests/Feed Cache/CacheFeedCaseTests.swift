d8498d3c14d75101c763ee97783389a4498e7a60
WeiTsungCheng <forwindsun1359@gmail.com>
2023-07-29 14:46:17 +0800

    Internal memory management and further refactorings for the Cache Feed Use Case

--- EssentialFeedStudyTests/Feed Cache/CacheFeedCaseTests.swift
+++ EssentialFeedStudyTests/Feed Cache/CacheFeedCaseTests.swift
@@ -20,7 +20,9 @@ class LocalFeedLoader {
     
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
         
-        store.deleteCachedFeed { [unowned self] error in
+        store.deleteCachedFeed { [weak self] error in
+            
+            guard let self = self else { return }
     
             if error == nil {
                 self.store.insert(items, timestamp: self.currentDate(), completion: completion)
@@ -114,6 +116,19 @@ final class CacheFeedCaseTests: XCTestCase {
       
     }
     
+    func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
+        
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults = [Error?]()
+        sut?.save([uniqueItem()]) { receivedResults.append($0) }
+        
+        sut = nil
+        store.completeDeletion(with: anyNSError())
+        XCTAssertTrue(receivedResults.isEmpty)
+        
+    }
     
     // MARK: - Helpers
     
