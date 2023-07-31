b1c3cd9753eb71b4aa0aaf94033c56c99baae551
WeiTsungCheng <forwindsun1359@gmail.com>
2023-07-31 10:27:06 +0800

    Move `LocalFeedItem` to its own file

---
+++ EssentialFeedStudy/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,24 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeedStudy
+//
+//  Created by WEI-TSUNG CHENG on 2023/7/31.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+    
+}
