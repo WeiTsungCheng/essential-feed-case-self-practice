daf3d05eb470ade326b96fb5c4d7647d194f7f03
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-06 12:01:19 +0800

    move `FeedCachePolicy` to it's own file

---
+++ EssentialFeedStudy/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeedStudy
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/6.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        
+        return date < maxCacheAge
+    }
+}
