e8d2a4d9edcd43648669529fee2c1e70cee5cbb6
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-21 01:20:42 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeedStudy/EssentailFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/20.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
