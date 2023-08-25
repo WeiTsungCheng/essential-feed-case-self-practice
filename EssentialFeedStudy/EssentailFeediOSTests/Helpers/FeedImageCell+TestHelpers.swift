dc805e05813a046d8f4017a7d458bf5651667fcd
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-25 12:36:41 +0800

    Extract FeedImageCell test helpers to a separate file

---
+++ EssentialFeedStudy/EssentailFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentailFeediOSTests
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/25.
+//
+
+import UIKit
+import EssentailFeediOS
+
+internal extension FeedImageCell {
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+    
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+
+    var locationText: String? {
+        return locationLabel.text
+    }
+    
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+}
