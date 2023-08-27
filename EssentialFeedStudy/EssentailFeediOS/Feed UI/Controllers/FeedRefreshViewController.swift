51b776e365093eef0db48a2f1f9e85fe94410fc5
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-27 19:57:57 +0800

    Remove mutable state from `FeedViewModel`. The `FeedViewModel` only needs to forward state changes, so state is only transient.

--- EssentialFeedStudy/EssentailFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -19,16 +19,16 @@ final class FeedRefreshViewController: NSObject {
     @objc func refresh() {
         viewModel.loadFeed()
     }
-
+    
     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        viewModel.onChange = { [weak self] viewModel in
-            if viewModel.isLoading {
-                self?.view.beginRefreshing()
+        viewModel.onLoadingStateChange = { [weak view] isLoading in
+            if isLoading {
+                view?.beginRefreshing()
             } else {
-                self?.view.endRefreshing()
+                view?.endRefreshing()
             }
         }
-
+        
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
     }
