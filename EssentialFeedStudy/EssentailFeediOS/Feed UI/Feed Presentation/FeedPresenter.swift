e0c94d48ad575c081b5fad12b4d0253d7f88b39d
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-28 16:47:16 +0800

    Add `FeedPresenter` holding feed loading presentation logic (loading state and loaded feed)

---
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,41 @@
+//
+//  FeedPresenter.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/28.
+//
+
+import Foundation
+import EssentialFeedStudy
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+    typealias Observer<T> = (T) -> Void
+    
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var feedView: FeedView?
+    weak var loadingView: FeedLoadingView?
+    
+    func loadFeed() {
+        loadingView?.display(isLoading: true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.loadingView?.display(isLoading: false)
+        }
+        
+    }
+}
