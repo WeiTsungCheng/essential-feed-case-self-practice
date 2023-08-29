fe97fb08bef1f18b9d7ff2c173cf4db9f8b657a2
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-29 21:12:40 +0800

    Move ViewModels to separate files

---
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/29.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/8/29.
+//
+
+import EssentialFeedStudy
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
