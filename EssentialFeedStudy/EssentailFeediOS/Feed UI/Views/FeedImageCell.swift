59b973aa5e553029d5aa81723ab71ef346e0e692
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-30 16:25:19 +0800

    iOS 15 Update releaseCellForReuse when cell prepareForReuse

--- EssentialFeedStudy/EssentailFeediOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -17,9 +17,15 @@ public class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) public var descriptionLabel: UILabel!
     
     var onRetry: (() -> Void)?
+    var onReuse: (() -> Void)?
     
     @IBAction private func retryButtonTapped() {
         onRetry?()
     }
     
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        onReuse?()
+    }
 }
