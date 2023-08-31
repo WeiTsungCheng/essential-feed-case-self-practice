fd0532c15f71e0b09d48c37f054bc077b737f6c8
WeiTsungCheng <forwindsun1359@gmail.com>
2023-08-31 16:08:34 +0800

    fix bug move extension static method from FeedPresenter to FeedViewController

--- EssentialFeedStudy/EssentailFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -14,7 +14,7 @@ public final class FeedUIComposer {
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
-        let feedController = FeedPresenter.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
+        let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
 
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
@@ -24,7 +24,7 @@ public final class FeedUIComposer {
     }
 }
 
-private extension FeedPresenter {
+private extension FeedViewController {
     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
