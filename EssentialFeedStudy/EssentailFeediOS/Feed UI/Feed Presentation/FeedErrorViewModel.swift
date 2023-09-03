ac3604b32b0b1d2080a6535e541d8ea94616e397
WeiTsungCheng <forwindsun1359@gmail.com>
2023-09-04 00:47:22 +0800

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/9/4.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
