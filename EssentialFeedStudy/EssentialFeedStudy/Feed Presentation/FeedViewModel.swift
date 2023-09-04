1b0d40be9c7ece13e066c1139a30daaadaded1e7
WeiTsungCheng <forwindsun1359@gmail.com>
2023-09-04 19:01:58 +0800

    Move `FeedViewModel` to separate file

---
+++ EssentialFeedStudy/EssentialFeedStudy/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeedStudy
+//
+//  Created by WEI-TSUNG CHENG on 2023/9/4.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
