750b9148eed38405dc5004cee9db42f44630f67b
WeiTsungCheng <forwindsun1359@gmail.com>
2023-09-07 13:49:20 +0800

    Extract duplicate `HTTPClientSpy` helper into a shared scope

---
+++ EssentialFeedStudy/EssentialFeedStudyTests/Feed API/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,48 @@
+//
+//  HTTPClientSpy.swift
+//  EssentialFeedStudyTests
+//
+//  Created by WEI-TSUNG CHENG on 2023/9/7.
+//
+
+import Foundation
+import EssentialFeedStudy
+
+class HTTPClientSpy: HTTPClient {
+    
+    private struct Task: HTTPClientTask {
+        let callBack: () -> Void
+        func cancel() { callBack() }
+    }
+    
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+    
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+    
+    var cancelledURLs = [URL]()
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        messages.append((url, completion))
+        
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil)!
+        
+        messages[index].completion(.success((data, response)))
+    }
+    
+}
