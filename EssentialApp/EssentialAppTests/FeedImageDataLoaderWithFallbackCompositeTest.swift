ec17795669e0d3732fda425f35beabe046bdd8a4
WeiTsungCheng <forwindsun1359@gmail.com>
2023-09-12 10:23:25 +0800

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTest.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTest.swift
@@ -10,6 +10,8 @@ import EssentialFeedStudy
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     
+    private let primary: FeedImageDataLoader
+    
     private class Task: FeedImageDataLoaderTask {
         func cancel() {
             
@@ -17,10 +19,12 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     }
     
     init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-        
+        self.primary = primary
     }
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeedStudy.FeedImageDataLoaderTask {
+        
+        _ = primary.loadImageData(from: url) { _ in }
         
         return Task()
     }
@@ -37,8 +41,26 @@ class FeedImageDataLoaderWithFallbackCompositeTest: XCTestCase {
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
+    
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        _ = sut.loadImageData(from: url, completion: { _ in })
+        
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
 
-    // MARK: - Helpers
+    }
+
+    // MARK: - Helpers'
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
+    }
+
     
     private class LoaderSpy: FeedImageDataLoader {
         
