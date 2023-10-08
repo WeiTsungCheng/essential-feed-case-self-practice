184440299474e61730ffc8b8526e03facff4ac05
WeiTsungCheng <forwindsun1359@gmail.com>
2023-10-08 15:57:29 +0800

    Replace if-try-statement with map

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -25,12 +25,10 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
-            
-            if let feed = try? result.get() {
+            completion(result.map{ feed in
                 self?.cache.save(feed) { _ in }
-            }
-            
-            completion(result)
+                return feed
+            })
         }
     }
 }
@@ -63,6 +61,7 @@ class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     func test_load_doesNotCacheOnLoaderFailure() {
         let cache = CacheSpy()
         let sut = makeSUT(loaderResult: .failure(anyNSError()), cache: cache)
+       
         sut.load { _ in }
         XCTAssertTrue(cache.messages.isEmpty, "Expected not to cache feed on load Error")
     }
