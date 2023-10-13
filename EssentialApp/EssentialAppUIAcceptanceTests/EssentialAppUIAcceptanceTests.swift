1a487f46bd7261b1784378c66e371526ae9e57f4
WeiTsungCheng <forwindsun1359@gmail.com>
2023-10-13 16:27:53 +0800

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by WEI-TSUNG CHENG on 2023/10/13.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+    
+}
