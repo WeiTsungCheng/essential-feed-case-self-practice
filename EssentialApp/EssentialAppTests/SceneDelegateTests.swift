ee12047bc1fee81bbb854eeeda571671f7e3d43a
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-06 19:52:58 +0800

    iOS 15 Update only check makeKeyAndVisible is called

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -12,13 +12,20 @@ import EssentailFeediOS
 class SceneDelegateTests: XCTestCase {
     
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        let window = UIWindow()
+        let window = UIWindowSpy()
+        
         let sut = SceneDelegate()
         sut.window = window
         
         sut.configureWindow()
-        
-        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+    
+    private class UIWindowSpy: UIWindow {
+      var makeKeyAndVisibleCallCount = 0
+      override func makeKeyAndVisible() {
+        makeKeyAndVisibleCallCount = 1
+      }
     }
 
     func test_configureWindow_configuresRootViewController() {
