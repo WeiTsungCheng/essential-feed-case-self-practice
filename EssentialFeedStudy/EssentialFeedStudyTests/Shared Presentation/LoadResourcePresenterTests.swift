7f82c9b1202d9ce3bd8fa1802ca4d6405a8bc94d
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-20 02:50:32 +0800

    Add FeedPresenter map

--- EssentialFeedStudy/EssentialFeedStudyTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeedStudy/EssentialFeedStudyTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -16,6 +16,14 @@ final class LoadResourcePresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_map_createsViewModel() {
+        let feed = uniqueImageFeed().models
+        
+        let viewModel = FeedPresenter.map(feed)
+
+        XCTAssertEqual(viewModel.feed, feed)
+    }
+    
     func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
         
