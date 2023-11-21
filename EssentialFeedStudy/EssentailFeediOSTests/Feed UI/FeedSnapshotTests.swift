c731274f0cffe6fc349deb6a373eedcd883172e4
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-22 00:43:33 +0800

    Group Feed UI tests in a new folder

--- EssentialFeedStudy/EssentailFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeedStudy/EssentailFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -16,7 +16,7 @@ class FeedSnapshotTests: XCTestCase {
         let sut = makeSUT()
         
         sut.display(emptyFeed())
-
+       
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
     }
