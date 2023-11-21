e0f371cfa4b9fee2c7efdf152379ea284e626a91
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-21 11:32:15 +0800

    Map ImageComments into ImageCommentsViewModels

--- EssentialFeedStudy/EssentialFeedStudyTests/Image Comments Presentation/ImageCommentsPresenterTest.swift
+++ EssentialFeedStudy/EssentialFeedStudyTests/Image Comments Presentation/ImageCommentsPresenterTest.swift
@@ -21,6 +21,40 @@ final class ImageCommentsPresenterTest: XCTestCase {
 
     }
     
+    func test_map_createsViewModel() {
+        let now = Date()
+        
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "a message",
+                createAt: now.adding(minutes: -5),
+                username: "a username"
+            ),
+            ImageComment(
+                id: UUID(),
+                message: "another message",
+                createAt: now.adding(days: -1),
+                username: "another username"
+            )
+        ]
+        
+        let viewModel = ImageCommentsPresenter.map(comments)
+        
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(
+                message: "a message",
+                date: "5 minutes ago",
+                username: "a username"
+            ),
+            ImageCommentViewModel(
+                message: "another message",
+                date: "1 day ago",
+                username: "another username"
+            )
+        ])
+    }
+    
     // MARK: - Helpers
     
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
