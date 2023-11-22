dca8bd56133fa628fca9c3f629edbccd31dd1924
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-22 18:58:13 +0800

    Implement Image Comments UI

---
+++ EssentialFeedStudy/EssentailFeediOS/Image Comments UI/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentailFeediOS
+//
+//  Created by WEI-TSUNG CHENG on 2023/11/22.
+//
+
+import UIKit
+
+public class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    
+}
