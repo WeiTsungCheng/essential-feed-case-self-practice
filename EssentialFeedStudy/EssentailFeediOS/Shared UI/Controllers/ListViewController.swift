29acb30cd340ea8b535ed3a69cf95bec1cc10c7b
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-24 21:56:39 +0800

    Configure `ErrorView` programmatically so we don't duplicate layout logic in storyboards

--- EssentialFeedStudy/EssentailFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeedStudy/EssentailFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -6,7 +6,7 @@ import UIKit
 import EssentialFeedStudy
 
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
-    @IBOutlet private(set) public var errorView: ErrorView?
+    private(set) public var errorView = ErrorView()
 
     private var loadingControllers = [IndexPath: CellController]()
     
@@ -20,6 +20,28 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         super.viewDidLoad()
         
         refresh()
+    }
+    
+    private func configureErrorView() {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(errorView)
+        
+        NSLayoutConstraint.activate([
+            errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: errorView.trailingAnchor),
+            errorView.topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: errorView.bottomAnchor),
+        ])
+        
+        tableView.tableHeaderView = container
+        
+        errorView.onHide = { [weak self] in
+            self?.tableView.beginUpdates()
+            self?.tableView.sizeTableHeaderToFit()
+            self?.tableView.endUpdates()
+        }
+
     }
     
     public override func viewDidLayoutSubviews() {
@@ -42,7 +64,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     }
     
     public func display(_ viewModel: ResourceErrorViewModel) {
-        errorView?.message = viewModel.message
+        errorView.message = viewModel.message
     }
 
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
