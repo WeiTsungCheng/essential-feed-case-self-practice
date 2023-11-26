eb58f1788fa96edbd5931f529a56dd87988b30ca
WeiTsungCheng <forwindsun1359@gmail.com>
2023-11-26 20:10:06 +0800

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeedStudy/EssentailFeediOS/Shared UI/Views/ErrorView.swift
+++ EssentialFeedStudy/EssentailFeediOS/Shared UI/Views/ErrorView.swift
@@ -29,15 +29,16 @@ public final class ErrorView: UIButton {
         backgroundColor = .errorBackgroundColor
         
         addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
-        configurable()
+        configureLabel()
         hideMessage()
     }
     
-    private func configurable() {
+    private func configureLabel() {
         titleLabel?.textColor = .white
         titleLabel?.textAlignment = .center
         titleLabel?.numberOfLines = 0
-        titleLabel?.font = .systemFont(ofSize: 17)
+        titleLabel?.font = .preferredFont(forTextStyle: .body)
+        titleLabel?.adjustsFontForContentSizeCategory = true
     }
     
     private var isVisible: Bool {
--- EssentialFeedStudy/EssentailFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeedStudy/EssentailFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -18,6 +18,7 @@ class ListSnapshotTests: XCTestCase {
 
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
+        
     }
 
     func test_listWithErrorMessage() {
@@ -27,6 +28,8 @@ class ListSnapshotTests: XCTestCase {
 
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
+
     }
 
     // MARK: - Helpers
