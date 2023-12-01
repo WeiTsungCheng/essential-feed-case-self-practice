b88d1cdc4bb6637fa9f177dd89aab14584ecc6d4
WeiTsungCheng <forwindsun1359@gmail.com>
2023-12-01 15:47:37 +0800

    Ensure previous cells' prepareForReuse doesn't affect new cells by removing the `onReuse` closure reference when releasing the cell for reuse.
    Every time we update the table view state, existing cells can be reused for different index paths, where they'll be managed by different cell controllers - so we need to remove all references to the previous cell controllers when releasing the cell for reuse. Since the `onReuse` closure references the cell controller via `self`, we need to set `onReuse` to `nil` when releasing the cell for reuse by another cell controller. This way, we ensure there are no references to the cell controller so there will be no side effects.

--- EssentialFeedStudy/EssentailFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeedStudy/EssentailFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -77,6 +77,7 @@ extension FeedImageCellController: UITableViewDataSource, UITableViewDelegate, U
     }
     
     private func releaseCellForReuse() {
+        cell?.onReuse = nil
         cell = nil
     }
 
