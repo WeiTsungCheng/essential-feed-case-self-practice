5856772f64464c3e859e4105c3f44cad9bf30b66
WeiTsungCheng <forwindsun1359@gmail.com>
2023-12-19 17:47:21 +0800

    fix rename variable name

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -16,21 +16,20 @@ public final class CommentsUIComposer {
     
     private typealias CommentPresentationAdapter = LoadResourcePresentationAdapter<[ImageComment], CommentsViewAdapter>
     
-    
     public static func commentsComposedWith(commentsLoader: @escaping () -> AnyPublisher<[ImageComment], Error>) -> ListViewController {
         let presentationAdapter = CommentPresentationAdapter(loader: commentsLoader)
         
-        let feedController = makeCommentsViewController(title: ImageCommentsPresenter.title)
-        feedController.onRefresh = presentationAdapter.loadResource
+        let commentsController = makeCommentsViewController(title: ImageCommentsPresenter.title)
+        commentsController.onRefresh = presentationAdapter.loadResource
         
         presentationAdapter.presenter = LoadResourcePresenter(
             resourceView: CommentsViewAdapter(
-                controller: feedController),
-            loadingView: WeakRefVirtualProxy(feedController),
-            errorView: WeakRefVirtualProxy(feedController),
+                controller: commentsController),
+            loadingView: WeakRefVirtualProxy(commentsController),
+            errorView: WeakRefVirtualProxy(commentsController),
             mapper: { ImageCommentsPresenter.map($0) })
         
-        return feedController
+        return commentsController
     }
     
     private static func makeCommentsViewController(title: String) -> ListViewController {
