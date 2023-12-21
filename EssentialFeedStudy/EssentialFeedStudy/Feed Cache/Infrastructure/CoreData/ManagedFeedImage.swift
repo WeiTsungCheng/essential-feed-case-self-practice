b031fb66dd01fd1d39637979aa7871b6c54359c2
WeiTsungCheng <forwindsun1359@gmail.com>
2023-12-21 13:26:14 +0800

    Optimize CoreData implementation to reduce cache misses

--- EssentialFeedStudy/EssentialFeedStudy/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeedStudy/EssentialFeedStudy/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,19 +8,20 @@
 import CoreData
 
 @objc(ManagedFeedImage)
-internal class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
     @NSManaged var data: Data?
     @NSManaged var cache: ManagedCache
-
+    
 }
 
 extension ManagedFeedImage {
     
     static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+        if let data = context.userInfo[url] as? Data { return data }
         return try first(with: url, in: context)?.data
     }
     
@@ -32,22 +33,28 @@ extension ManagedFeedImage {
         return try context.fetch(request).first
     }
     
-    internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-        return NSOrderedSet(array: localFeed.map { local in
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        let images = NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
             managed.imageDescription = local.description
             managed.location = local.location
             managed.url = local.url
+            managed.data = context.userInfo[local.url] as? Data
             return managed
         })
+        context.userInfo.removeAllObjects()
+        
+        return images
     }
-
+    
     internal var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
+    
+    override func prepareForDeletion() {
+        super.prepareForDeletion()
+        
+        managedObjectContext?.userInfo[url] = data
+    }
 }
-
-
-
-
